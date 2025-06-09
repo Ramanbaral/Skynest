4e719c05951c36c8f80f8dcb1c9611d571c425d6
raman baral <ramanisstudent@gmail.com>
2025-06-09 15:38:11 +0545

    modify readme.md

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "SKYNEST",
-  description: "Skynest is a modern cloud-based platform that allows users to seamlessly upload, organize, and manage their digital content. Designed with simplicity and efficiency in mind, Skynest supports secure storage of images and PDFs, giving users the ability to create folders, upload files, and keep their documents neatly organized in one place.",
+  description: "Skynest is a cloud-based platform that allows users to seamlessly upload, organize, and manage their digital content. Designed with simplicity and efficiency in mind, Skynest supports secure storage of images and PDFs, giving users the ability to create folders, upload files, and keep their documents neatly organized in one place.",
 };
 
 export default function RootLayout({
