a93477cf8eef3aac611969913e7208ed305e0663
raman baral <ramanisstudent@gmail.com>
2025-06-09 15:33:23 +0545

    create schema for file

---
+++ src/db/schema.ts
@@ -0,0 +1,37 @@
+import { relations } from "drizzle-orm";
+import {
+  pgTable,
+  integer,
+  varchar,
+  boolean,
+  uuid,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const filesTable = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name").notNull(),
+  path: varchar("path").notNull(),
+  size: integer("size").notNull(),
+  type: varchar("type").notNull(), //"folder"
+  fileUrl: varchar("file_url").notNull(),
+  thumbnailUrl: varchar("thumbnail_url"),
+  userId: varchar("user_id").notNull(),
+  parentId: uuid("parent_id"),
+  isFolder: boolean("is_folder").default(false).notNull(),
+  isStarred: boolean("is_starred").default(false).notNull(),
+  isTrash: boolean("is_trash").default(false).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const filesRelations = relations(filesTable, ({ one, many }) => ({
+  parent: one(filesTable, {
+    fields: [filesTable.parentId],
+    references: [filesTable.id],
+  }),
+  children: many(filesTable),
+}));
+
+export const File = typeof filesTable.$inferSelect;
+export const InsertFile = typeof filesTable.$inferInsert;
